2b2c23faf1ac611b574ff9085301d62a6a824b27
Shivam1613 <shivam.dubey@seventhtriangle.com>
2025-12-19 10:38:52 +0530

    changes

--- src/utils/openAIi.js
+++
@@ -1,9 +0,0 @@
-// import OpenAI from 'openai';
-// import {OPENAI_KEY} from "./constants";
-
-// const openAi = new OpenAI({
-//   apiKey: OPENAI_KEY, // This is the default and can be omitted
-//   dangerouslyAllowBrowser: true,
-// });
-
-// export default openAi;
